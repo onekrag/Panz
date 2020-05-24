4a527c6caa75b73ce0f71a7f9d2aa9cbf79396d0
Andrey <rumpel45@ya.ru>
2020-05-24 15:45:34 +0500

    123133

--- js/script.js
+++ js/script.js
@@ -19,26 +19,5 @@
 
     $('.langrage__box').click(function (event) {
        $('.langrage__box-eu,.langrage__box-na').toggleClass('langrage__box-active');
-       // css( 'display', 'block');
-
     });
-
-
-
-
-
-
-
-
-
  })
-
- // new fullpage('#fullpage', {
- //     licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
- // 	//options here
- // 	autoScrolling:true,
- // 	scrollHorizontally: true
- // });
-
- // //methods
- // fullpage_api.setAllowScrolling(false);
