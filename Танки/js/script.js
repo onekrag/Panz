9b694c7cb76f57cb186bb26fc9d56d254afa4625
Andrey <rumpel45@ya.ru>
2020-05-24 12:18:19 +0500

    1212

--- Танки/js/script.js
+++ Танки/js/script.js
@@ -1,38 +1,44 @@
  $(document).ready(function () {
 
+    $('#fullpage').fullpage({
+       slidesNavigation: true,
+       // slidesNavPosition: 'bottom',
+       anchors: ['firstPage', 'secondPage', 'thirdPage']
+    });
 
-    
+    $(".scrollpanel").scrollpanel();
 
+    $(".scrollpanel").scrollpanel({
+       prefix: 'sp-'
+    });
 
-     $('#fullpage').fullpage({
-        slidesNavigation: true,
-        // slidesNavPosition: 'bottom',
-        anchors:['firstPage', 'secondPage', 'thirdPage']
-     });
-
-     $(".scrollpanel").scrollpanel();
-
-     $(".scrollpanel").scrollpanel({
-        
-              prefix: 'sp-'
-        
-            });
-
-     
+    $(function () {
+       $("#my-accordion").accordion();
+    });
 
 
+    $('.langrage__box').click(function (event) {
+       $('.langrage__box-eu,.langrage__box-na').toggleClass('langrage__box-active');
+       // css( 'display', 'block');
+
+    });
 
 
-     
-     
+
+
+
+
+
+
+
  })
 
-// new fullpage('#fullpage', {
-//     licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-// 	//options here
-// 	autoScrolling:true,
-// 	scrollHorizontally: true
-// });
+ // new fullpage('#fullpage', {
+ //     licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+ // 	//options here
+ // 	autoScrolling:true,
+ // 	scrollHorizontally: true
+ // });
 
-// //methods
-// fullpage_api.setAllowScrolling(false);
+ // //methods
+ // fullpage_api.setAllowScrolling(false);
